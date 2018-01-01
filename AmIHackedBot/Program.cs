d275734cb92af46a50f1629697c2cf9e919b259b
Simplevolk <simplevolk@gmail.com>
2018-01-01 03:57:49 +0300

    Add logging

--- AmIHackedBot/Program.cs
+++ AmIHackedBot/Program.cs
@@ -4,7 +4,7 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
-
+using Microsoft.Extensions.Logging;
 namespace AmIHackedBot
 {
     class Program
@@ -27,57 +27,64 @@ namespace AmIHackedBot
         /// <param name="e"></param>
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            if (e.Message.Text.StartsWith("/start"))
+            try
             {
-                var msg = new StringBuilder();
-                msg.AppendLine("Welcome!");
-                msg.AppendLine("This bot works with www.haveibeenpwned.com");
-                msg.AppendLine("Type your email to find are your hacked");
-
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
-                return;
-            }
-            else if (e.Message.Text.StartsWith("/email"))
-            {
-                var msg = "Type your email to find are your hacked";
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg, Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
-                return;
-            }
-
-            if (string.IsNullOrEmpty(e.Message.Text))
-                return;
-
-            var regexUtils = new RegexUtilities();
-            if (!regexUtils.IsValidEmail(e.Message.Text))
-            {
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, $"'{e.Message.Text}' is not valid email!", Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
-                return;
-            }
-
-            var client = new HaveIBeenPwnedRestClient();
-            var response = client.GetAccountBreaches(e.Message.Text).Result;
-            foreach (Breach x in response)
-            {
-                var msg = new StringBuilder();
-                msg.AppendLine($"Domain: {x.Domain}");
-                msg.AppendLine($"Breached Data:{x.BreachDate}");
-                msg.AppendLine($"Description: {x.Description}");
-
-                if (x.DataClasses.Count != 0)
+                if (e.Message.Text.StartsWith("/start"))
                 {
-                    var dataClasses = new StringBuilder();
-                    foreach (var dc in x.DataClasses)
-                    {
-                        dataClasses.Append($"{dc}\t");
-                    }
-                    msg.AppendLine(dataClasses.ToString());
-                }
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Html, false, false);
-            }
+                    var msg = new StringBuilder();
+                    msg.AppendLine("Welcome!");
+                    msg.AppendLine("This bot works with www.haveibeenpwned.com");
+                    msg.AppendLine("Type your email to find are your hacked");
 
-            if(response.Count==0)
+                    BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
+                    return;
+                }
+                else if (e.Message.Text.StartsWith("/email"))
+                {
+                    var msg = "Type your email to find are your hacked";
+                    BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg, Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(e.Message.Text))
+                    return;
+
+                var regexUtils = new RegexUtilities();
+                if (!regexUtils.IsValidEmail(e.Message.Text))
+                {
+                    BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, $"'{e.Message.Text}' is not valid email!", Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                    return;
+                }
+
+                var client = new HaveIBeenPwnedRestClient();
+                var response = client.GetAccountBreaches(e.Message.Text).Result;
+                foreach (Breach x in response)
+                {
+                    var msg = new StringBuilder();
+                    msg.AppendLine($"Domain: {x.Domain}");
+                    msg.AppendLine($"Breached Data:{x.BreachDate}");
+                    msg.AppendLine($"Description: {x.Description}");
+
+                    if (x.DataClasses.Count != 0)
+                    {
+                        var dataClasses = new StringBuilder();
+                        foreach (var dc in x.DataClasses)
+                        {
+                            dataClasses.Append($"{dc}\t");
+                        }
+                        msg.AppendLine(dataClasses.ToString());
+                    }
+                    BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Html, false, false);
+                }
+
+                if (response.Count == 0)
+                {
+                    BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, $"E-mail is clean!", Telegram.Bot.Types.Enums.ParseMode.Html, false, false);
+                }
+            }
+            catch(Exception ex)
             {
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, $"E-mail is clean!", Telegram.Bot.Types.Enums.ParseMode.Html, false, false);
+                StaticUtils.Logger.LogError(ex, ex.Message);
             }
         }
     }
---
+++ AmIHackedBot/StaticUtils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AmIHackedBot
+{
+    public static class StaticUtils
+    {
+        private static ILoggerFactory LoggerFactory;
+
+        public static ILogger Logger;
+
+        static StaticUtils()
+        {
+            var loggerPath = Path.Combine(Directory.GetCurrentDirectory(), "logger.txt");
+            LoggerFactory = new LoggerFactory();
+            LoggerFactory.AddFile(loggerPath);            
+            Logger = StaticUtils.LoggerFactory.CreateLogger<Program>();
+        }
+    }
+}
