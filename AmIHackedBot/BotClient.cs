7e45bf14138cb15f6ccdda84adaf78e1fcce3e5f
Simplevolk <simplevolk@gmail.com>
2018-01-01 03:37:05 +0300

    first alpha version. add request to service

--- AmIHackedBot/BotClient.cs
+++ AmIHackedBot/BotClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using Telegram.Bot;
 
@@ -9,13 +10,19 @@ namespace AmIHackedBot
     public sealed class BotClient : TelegramBotClient
     {
         private static readonly Lazy<BotClient> lazy =
-            new Lazy<BotClient>(() => new BotClient(File.ReadAllText("Config.txt")));
+            new Lazy<BotClient>(() => new BotClient(File.ReadAllText(Path.Combine(GetExecutingDirectoryName(), "Config.txt"))));
 
         public static BotClient Instance { get { return lazy.Value; } }
 
         private BotClient(string token)
         : base(token)
         {
+        }
+
+        private static string GetExecutingDirectoryName()
+        {
+            var location = new Uri(Assembly.GetEntryAssembly().GetName().CodeBase);
+            return new FileInfo(location.AbsolutePath).Directory.FullName;
         }
     }
 }
--- AmIHackedBot/Program.cs
+++ AmIHackedBot/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SharpPwned.NET;
+using SharpPwned.NET.Model;
+using System;
 using System.Text;
+using System.Threading.Tasks;
 using Telegram.Bot;
 
 namespace AmIHackedBot
@@ -7,11 +10,15 @@ namespace AmIHackedBot
     class Program
     {
         private static TelegramBotClient Bot = BotClient.Instance;
-        static void Main(string[] args)
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        private async Task MainAsync()
         {
             Bot.OnMessage += Bot_OnMessage;
             Console.WriteLine("Hello World!");
+            Bot.StartReceiving();
+            await Task.Delay(-1);
         }
+
 
         /// <summary>
         /// Handle messages from user
@@ -28,11 +35,44 @@ namespace AmIHackedBot
                 msg.AppendLine("Type your email to find are your hacked");
 
                 BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
+                return;
             }
-            else if(e.Message.Text.StartsWith("/email"))
+            else if (e.Message.Text.StartsWith("/email"))
             {
                 var msg = "Type your email to find are your hacked";
-                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg, Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
+                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg, Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(e.Message.Text))
+                return;
+
+            var regexUtils = new RegexUtilities();
+            if (!regexUtils.IsValidEmail(e.Message.Text))
+            {
+                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, $"'{e.Message.Text}' is not valid email!", Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                return;
+            }
+
+            var client = new HaveIBeenPwnedRestClient();
+            var response = client.GetAccountBreaches(e.Message.Text).Result;
+            foreach (Breach x in response)
+            {
+                var msg = new StringBuilder();
+                msg.AppendLine($"Domain: {x.Domain}");
+                msg.AppendLine($"Breached Data:{x.BreachDate}");
+                msg.AppendLine($"Description: {x.Description}");
+
+                if (x.DataClasses.Count != 0)
+                {
+                    var dataClasses = new StringBuilder();
+                    foreach (var dc in x.DataClasses)
+                    {
+                        dataClasses.Append($"{dc}\t");
+                    }
+                    msg.AppendLine(dataClasses.ToString());
+                }
+                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Html, false, false);
             }
         }
     }
