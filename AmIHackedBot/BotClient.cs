902be3e97ea2afa44f35b1875ea304147c806f02
Simplevolk <simplevolk@gmail.com>
2018-01-01 03:11:10 +0300

    init

---
+++ AmIHackedBot/BotClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Telegram.Bot;
+
+namespace AmIHackedBot
+{
+    public sealed class BotClient : TelegramBotClient
+    {
+        private static readonly Lazy<BotClient> lazy =
+            new Lazy<BotClient>(() => new BotClient(File.ReadAllText("Config.txt")));
+
+        public static BotClient Instance { get { return lazy.Value; } }
+
+        private BotClient(string token)
+        : base(token)
+        {
+        }
+    }
+}
--- AmIHackedBot/Program.cs
+++ AmIHackedBot/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using Telegram.Bot;
 
 namespace AmIHackedBot
 {
     class Program
     {
+        private static TelegramBotClient Bot = BotClient.Instance;
         static void Main(string[] args)
         {
+            Bot.OnMessage += Bot_OnMessage;
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Handle messages from user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Text.StartsWith("/start"))
+            {
+                var msg = new StringBuilder();
+                msg.AppendLine("Welcome!");
+                msg.AppendLine("This bot works with www.haveibeenpwned.com");
+                msg.AppendLine("Type your email to find are your hacked");
+
+                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg.ToString(), Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
+            }
+            else if(e.Message.Text.StartsWith("/email"))
+            {
+                var msg = "Type your email to find are your hacked";
+                BotClient.Instance.SendTextMessageAsync(e.Message.From.Id, msg, Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false, e.Message.MessageId);
+            }
         }
     }
 }
