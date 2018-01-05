f030a96d6239a54d8ae0f5fbec2fd3f63ddce273
Simplevolk <simplevolk@gmail.com>
2018-01-06 00:30:49 +0300

    add notification

--- AmIHackedBot/Emails/UpdateService.cs
+++ AmIHackedBot/Emails/UpdateService.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
+using AmIHackedBot.Sharp;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -23,10 +26,22 @@ namespace AmIHackedBot.Emails
         /// </summary>
         private EmailManager _emailManager;
 
+        /// <summary>
+        /// Client from API
+        /// </summary>
+        private HaveIBeenPwnedRestClient _client;
+        /// <summary>
+        /// Notification client
+        /// </summary>
+        private Notification _notification;
+
         public UpdateService(EmailManager emailManager)
         {
-            _sub = Observable.Interval(TimeSpan.FromDays(1));//one request per day
+            _client = new HaveIBeenPwnedRestClient();
             _emailManager = emailManager;
+            _notification = new Notification();
+            _sub = Observable.Interval(TimeSpan.FromMinutes(1));//one request per day
+
         }
 
         /// <summary>
@@ -54,17 +69,59 @@ namespace AmIHackedBot.Emails
             foreach (var telegramId in _emailManager.TelegramIdToEmailDict.Keys)
             {
                 var userFile = Path.Combine($"{SharedData.EmailsDirectory}", $"{telegramId}.txt");
-                using (StreamWriter file =
-       new StreamWriter($"{userFile}"))
+                var emailsColl = _emailManager.TelegramIdToEmailDict[telegramId];
+                var removeEmails = new List<Email>();
+                var addedEmails = new List<Email>();
+                foreach (var email in emailsColl)
                 {
-                    var user = new User(telegramId, _emailManager.TelegramIdToEmailDict[telegramId]);
-                    var userString = JsonConvert.SerializeObject(user);
+                    try
+                    {
+                        StaticUtils.Logger.LogInformation($"Try to find email '{email.Name}' from Pwned databases");
+                        var response = _client.GetAccountBreaches(email.Name).Result;
+                        if (email.BreachColl.Count != response.Count)//need to send notification
+                        {
+                            _notification.SendNotification(telegramId, email.Name, email.BreachColl, response);
+                            var newEmail = new Email(email.Name, response);
+                            removeEmails.Add(email);
+                            addedEmails.Add(newEmail);
+                        }
+                        StaticUtils.Logger.LogInformation($"Email '{email.Name}' Old breaches:'{email.BreachColl.Count}' New breaches:'{response.Count}'");
+                    }
+                    catch (Exception ex)
+                    {
+                        StaticUtils.Logger.LogError(ex, "Error while get result from API");
+                    }
+                }
+
+                if (removeEmails.Any())
+                {
+                    foreach (var remEm in removeEmails)
+                    {
+                        _emailManager.TelegramIdToEmailDict[telegramId].Remove(remEm);
+                    }
+                }
+
+                if (addedEmails.Any())
+                {
+                    foreach (var add in addedEmails)
+                    {
+                        _emailManager.TelegramIdToEmailDict[telegramId].Add(add);
+                    }
+                }
+
+                using (StreamWriter file =
+                     new StreamWriter($"{userFile}"))
+                {
+                    var forwardUser = new User(telegramId, emailsColl);
+                    var userString = JsonConvert.SerializeObject(forwardUser);
                     if (userString != null)
                         file.WriteLine(userString);
                 }
+
+
+                sw.Stop();
+                StaticUtils.Logger.LogInformation($"Finish to update pwned emails:'{sw.ElapsedMilliseconds}' ms");
             }
-            sw.Stop();
-            StaticUtils.Logger.LogInformation($"Finish to update pwned emails:'{sw.ElapsedMilliseconds}' ms");
         }
     }
 }
---
+++ AmIHackedBot/Notification.cs
@@ -0,0 +1,42 @@
+﻿using AmIHackedBot.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AmIHackedBot
+{
+    /// <summary>
+    /// Class of notification
+    /// </summary>
+    internal class Notification
+    {
+        /// <summary>
+        /// Send notification to User
+        /// </summary>
+        /// <param name="telegramId">user's telegram id</param>
+        /// <param name="oldBreaches">old breaches coll</param>
+        /// <param name="newBreaches">new breaches coll</param>
+        ///<param name="email">email</param>
+        public void SendNotification(long telegramId,string email, List<Breach> oldBreaches, List<Breach> newBreaches)
+        {
+            var firstNotSecond = newBreaches.Except(oldBreaches).ToList();
+            if (!firstNotSecond.Any())
+            {
+                BotClient.Instance.SendTextMessageAsync(telegramId, $"Warning! Your mail  '{email}' has appeared in the bases of hackers!",
+                    Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                foreach (var res in firstNotSecond)
+                {
+                    var msg = new StringBuilder();
+                    msg.AppendLine($"Domain: {res.Domain}");
+                    msg.AppendLine($"Breached Data:{res.BreachDate}");
+                    msg.AppendLine($"Description: {res.Description}");
+                    BotClient.Instance.SendTextMessageAsync(telegramId, $"{msg.ToString()}",
+                    Telegram.Bot.Types.Enums.ParseMode.Markdown, false, false);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
